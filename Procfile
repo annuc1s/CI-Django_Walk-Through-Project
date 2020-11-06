3f8670f0385a2963f5d1f873ee48b706b22e7ecc
annuc1s <annuciis121@gmail.com>
2020-11-06 16:56:24 +0000

    Deployment to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn boutique_store.wsgi:application
--- boutique_store/settings.py
+++ boutique_store/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '4-&o+pm1fq4*4-x6+5yw)2gsq+iay38_bz@pl^v4=uoyd5_r_2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['walkkthrough-django-ecommerce.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,12 +120,17 @@ WSGI_APPLICATION = 'boutique_store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
